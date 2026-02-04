1f582bee90a78d1a71a3df2dc3e1a4487ee0649d
noemi-desing <noemi.rox011@gmail.com>
2026-02-04 17:20:20 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function loadCatalogs() {
+  alert("Catalogs module opened.");
+}
+function openInventory() {
+  alert("Inventory module opened.");
+}
+function manageAssignments() {
+  alert("Assignments module opened.");
+}
+function generateReports() {
+  alert("Reports generated successfully.");
+}
